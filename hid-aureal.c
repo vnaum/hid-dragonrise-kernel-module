9396fe24fd6d1c2879a31826b25c096482ce224b
Vladislav Naumov <vnaum@vnaum.com>
2011-01-06 20:51:43 +0600

    ok, one more excess field left (3 axis)

--- hid-aureal.c
+++ hid-aureal.c
@@ -32,7 +32,24 @@ static void aureal_report_fixup(struct hid_device *hdev, __u8 *rdesc, unsigned i
 	if (rsize == 101 && rdesc[0x17] == 0x30 && rdesc[0x19] == 0x30 && rdesc[0x1b] == 0x30) {
 		dev_info(&hdev->dev, "fixing DragonRise "
 				"report descriptor. Just 2 axis on this device.\n");
-		rdesc[0x17] = rdesc[0x19] = rdesc[0x1b] = 0;
+
+		rdesc[11] = 0x03; //         Report Size (8),
+		rdesc[12] = 0x81; //         Report Count (3),
+		rdesc[13] = 0x01; //         Input (Constant),
+		rdesc[14] = 0x75; //         Report Size (8),
+		rdesc[15] = 0x08; //         Report Count (2),
+		rdesc[16] = 0x95; //         Logical Minimum (0),
+		rdesc[17] = 0x02; //         Logical Maximum (255),
+		rdesc[18] = 0x15; //         Physical Minimum (0),
+		rdesc[19] = 0x00; //         Physical Maximum (255),
+		rdesc[20] = 0x26; //         Usage (X),                      ; X (30h, dynamic value)
+		rdesc[21] = 0xFF; //         Usage (Y),                      ; Y (31h, dynamic value)
+		rdesc[22] = 0x00; //         Input (Variable),
+		rdesc[23] = 0x35;
+		rdesc[24] = 0x00;
+		rdesc[25] = 0x46;
+		rdesc[26] = 0xFF;
+		rdesc[27] = 0x00;
 	}
 }
 
